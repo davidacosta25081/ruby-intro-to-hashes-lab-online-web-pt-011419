8130ac333d4c58751bb26cb8f2af1ee33fce337f
David Acosta <davidacosta25081@yahoo.com>
2019-02-06 03:47:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,15 +10,18 @@ def monopoly
 	monopoly = {
 		:railroads => {}
 	}
-	empty
+	
 	
 end
 
 def monopoly_with_second_tier
+	monopoly = {
+		:railroads => {
+:pieces => 4, :rent_in_dollars =>{},:names =>{}
 
-
-}
-
+			
+		}
+	}
 end
 
 def monopoly_with_third_tier
